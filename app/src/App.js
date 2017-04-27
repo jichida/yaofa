1f018dfc2f0c53a64442f91faa0e0850c5f6513c
Jiaowenhui <523947655@qq.com>
2017-04-27 19:59:48 +0800

    界面登录、注册、发布借款

--- app/src/App.js
+++ app/src/App.js
@@ -1,21 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import DocumentTitle from "react-document-title";
+import Login from "./components/login";
+import Register from "./components/register";
+import AddBorrow from "./components/user/addborrow";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        let indexbanner = ["img/5.png"];
+        return (
+            <div className="AppContainer">
+                <AddBorrow />
+
+            </div>
+        );
+    }
 }
 
 export default App;
