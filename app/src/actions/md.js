79505e0c5836c5f076db80bb8c135fd7a7bd8589
downup2u <25199344@qq.com>
2017-05-08 19:44:22 +0800

    md_loginsendauth_result

--- app/src/actions/md.js
+++ app/src/actions/md.js
@@ -1,3 +1,4 @@
 import { createAction } from 'redux-act';
 
 export const md_login_result = createAction('md_login_result');
+export const md_loginsendauth_result =  createAction('md_loginsendauth_result');
