09cf581a99f0d6876cdb6acc997fd405eaea3f6e
Jiaowenhui <523947655@qq.com>
2017-05-15 20:39:29 +0800

    补文件

---
+++ app/src/components/agency/tixian3.js
@@ -0,0 +1,89 @@
+import React, { Component, PropTypes } from 'react';
+import '../../../public/css/tixian.css';
+import { connect } from 'react-redux';
+import DocumentTitle from "react-document-title";
+import {
+    profit_set_profityanzhen,
+    set_weui,
+    sendauth_request,
+    withdrawcashapplyauth_request,
+} from '../../actions';
+
+export class Page extends Component {
+
+    constructor(props) {  
+        super(props);  
+        this.getYanZhenCodeInterval = null;
+        this.state = {
+            getYanZhenCodeText : "重新获取",
+            getYanZhenCodeTime : 60,
+        }
+    } 
+
+    onClickReturn =()=>{
+        this.props.history.goBack();
+    }
+    
+    componentWillMount() {
+        this.getYanZhenCode();
+    }
+
+    getYanZhenCode=()=>{
+        this.props.dispatch(sendauth_request({phonenumber: this.props.username}));
+    }
+
+    //重新获取验证码
+    getSendCode =()=>{
+        this.props.dispatch(sendauth_request({username: this.props.username}));
+    }
+
+    submitProfitform =()=>{
+        let form = this.props.profitform;
+        let payload = {
+            _id: this.props.profitid,
+            username: this.props.username,
+            authcode: this.props.profityanzhen
+        };
+        this.props.dispatch(withdrawcashapplyauth_request(payload));
+        
+    }
+
+    setYanzhen =(e)=>{
+        let val = e.target.value;
+        this.props.dispatch(profit_set_profityanzhen(val));
+    }
+
+    render() {
+        let showphonenumber = this.props.username.substr(0,3)+"****"+this.props.username.substr(7,4);
+        return (
+            <div className="messageCode AppPage">
+                <DocumentTitle title="提现验证" />
+                <div className="messageCodeContent">
+                    <span className="tit">请输入<span className="phone">{showphonenumber}</span>收到的短信验证码</span>
+                    <div className="messageCodeInput">
+                        <span className="txt">验证码</span>
+                        <input placeholder='请输入验证码' onChange={(e)=>{this.setYanzhen(e)}} />
+                        <span 
+                            className="getcode" 
+                            onClick={()=>{this.getSendCode()}}>
+                            {this.state.getYanZhenCodeText}
+                        </span>
+                    </div>
+
+                </div>
+                <div className="buttoncon">
+                    <button 
+                        className="btn Primary" onClick={()=>{this.submitProfitform()}}
+                        style={{width:"100%"}}
+                    >
+                        完成
+                    </button>
+                </div>
+            </div>
+        );
+    }
+}
+const data =  ({userlogin,profit}) =>{ return {...userlogin, ...profit};};
+export default connect(data)(Page);
+
+
---
+++ app/src/components/boss/pay.js
@@ -0,0 +1,133 @@
+/*
+    借款信息
+*/
+import React, { Component } from 'react';
+import DocumentTitle from "react-document-title";
+import WeUI from 'react-weui';
+import 'weui';
+import 'react-weui/lib/react-weui.min.css';
+import '../../../public/css/index.css';
+import Footer from './footer';
+import _ from "lodash";
+import { connect } from "react-redux";
+import SwiperBanner from '../tools/swiperbanner';
+import moment from 'moment';
+import {
+    set_lender_borrowlist,
+    set_lender_borrowlist_filler,
+    queryintrestedorder_request,
+    set_orderinfo,
+    set_weui
+} from '../../actions';
+
+const { 
+    Tab,
+    NavBar,
+    NavBarItem,
+    TabBody,
+    Cells,
+    Cell,
+    CellHeader,
+    CellBody,
+    CellFooter,
+    Form,
+    FormCell,
+    Select,
+    Label,
+    Input,
+    Switch,
+    LoadMore
+    } = WeUI;
+
+class Page extends Component {
+
+    headBanner =()=>{
+        let data = [
+            "img/5.png",
+            "img/5.png",
+        ]
+        return data;
+    }
+
+    pushUrl = (name)=>{
+        this.props.history.push(name);
+    }
+
+    componentWillMount() {
+        this.getList();
+    }
+
+    gotoBorrowInfo =(borrowinfo)=>{
+        this.props.dispatch(set_orderinfo(borrowinfo));
+        this.pushUrl("/borrowinfo");
+    }
+
+    getList =()=>{
+        let query = this.props.borrowlistfiller;
+        this.props.dispatch(queryintrestedorder_request({query}))
+    }
+
+	render() {
+        const {borrowlist,borrowlistfiller} = this.props;
+        return (
+            <div className="indexPage AppPage">
+                <DocumentTitle title="耀发钱庄－放款端" />
+        		<SwiperBanner data={this.headBanner()} />
+                <div className="pageTitle bossindexfiller">
+                    <span>最新借款信息</span>
+                    <span 
+                        onClick={()=>{this.pushUrl("/bossfiller")}}
+                        className="filler sel">
+                        筛选
+                        <img src="img/7.png" />
+                    </span>
+                </div>
+                <div className="list">
+                    {borrowlist.length>0?(
+                        <Cells>
+                            {
+                                _.map(borrowlist, (borrow,index)=>{
+                                    return (
+                                        <Cell 
+                                            access
+                                            key={index}
+                                            onClick={()=>{this.gotoBorrowInfo(borrow);}}
+                                            >
+                                            <CellHeader>
+                                                <img src="img/6.png" alt="" />
+                                                <div className="userinfo">
+                                                    <span className="name">borrow.created.profile.nickname</span>
+                                                    <span className="time">借款期限: {borrow.moneyperiod}</span>
+                                                    <span className="time">发布时间: {moment(borrow.created_at).format('YYYY-MM-DD H:mm:ss')}</span>
+                                                    
+                                                </div>
+                                            </CellHeader>
+                                            <CellBody>
+                                                <div>借 <span className="blue">{borrow.moneylimit}</span> 元</div>
+                                                <div>{borrow.statusforlender}</div>
+                                            </CellBody>
+                                            <CellFooter/>
+                                        </Cell>
+                                    )
+                                })
+                            }
+                        </Cells>
+                    ):(
+                        <LoadMore showLine>暂无数据</LoadMore>
+                    )}
+                    
+                </div>
+                <Footer action={0}/>
+            </div>
+    	)
+    }
+}
+
+const data = ({userlender:{borrowlist,borrowlistfiller}}) => {
+    borrowlist = _.sortBy(borrowlist, [function(o) { return -(new Date(o.created_at)).getTime(); }]);
+    return {borrowlist,borrowlistfiller};
+};
+Page = connect(data)(Page);
+export default Page;
+
+
