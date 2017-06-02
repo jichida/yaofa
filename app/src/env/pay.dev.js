91dc685dea1bd961635b2e67515c15d6f9b583fe
Jiaowenhui <523947655@qq.com>
2017-06-03 00:18:35 +0800

    no message

--- app/src/env/pay.dev.js
+++ app/src/env/pay.dev.js
@@ -5,7 +5,7 @@ import {
 
 export const payorder = (paysign,orderinfo,callbackfn)=>{
   alert("JSON.stringify(paysign)"+JSON.stringify(paysign));
-  console.log("JSON.stringify(paysign)"+JSON.stringify(paysign));
+  console.log(paysign);
   let postdata = {
       "out_trade_no":orderinfo._id
   };
@@ -15,8 +15,16 @@ export const payorder = (paysign,orderinfo,callbackfn)=>{
   //     callbackfn(result);
   // });
   let onBridgeReady =()=>{
+      console.log("onBridgeReady::::"+JSON.stringfiy(paysign));
      window.WeixinJSBridge.invoke(
-         'getBrandWCPayRequest', paysign,
+         'getBrandWCPayRequest', {
+            "appId": paysign.appId,     //公众号名称，由商户传入     
+            "timeStamp": paysign.timeStamp,         //时间戳，自1970年以来的秒数     
+            "nonceStr": paysign.nonceStr, //随机串     
+            "package": paysign.package,     
+            "signType": "MD5",         //微信签名方式：     
+            "paySign": paysign.paySign //微信签名 
+         },
          function(res){     
              if(res.err_msg === "get_brand_wcpay_request:ok" ) {}     // 使用以上方式判断前端返回,微信团队郑重提示：res.err_msg将在用户支付成功后返回    ok，但并不保证它绝对可靠。 
          }
