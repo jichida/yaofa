e02f3bf1994413e336c4bb2b4c2a72c234f906b2
Jiaowenhui <523947655@qq.com>
2017-06-13 16:47:06 +0800

    no message

--- app/src/env/config.prod.js
+++ app/src/env/config.prod.js
@@ -2,7 +2,7 @@ let config = {
   serverurl:'http://wx.mrtejia.cn',
   requesttimeout:5000,
   appversion : 1.0,
-  refreshmyorderinterval:5000,//5秒
+  refreshmyorderinterval:5000*10,//5秒
   refreshsysconfiginterval:60000*5,//5分钟
   appid : "wx8ec8ba53700c0c89",
   redirect_uri : "http://wx.mrtejia.cn/app/getopenid",
