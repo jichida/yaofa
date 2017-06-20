66e45cbaa7149b8a3905edb12917a93d604aa37a
wangxiaoqing <xiaoqingwang@126.com>
2017-06-20 20:37:45 +0800

    loading. 在第6行和第93行加入你需要的action.

--- app/src/sagas/index.js
+++ app/src/sagas/index.js
@@ -3,8 +3,10 @@ import {flowmain} from './flowmain';
 import {wsrecvsagaflow} from './wsrecvsaga';
 import {payflow} from './payflow';
 import {refreshmyorderflow,refreshsystemconfig} from './refreshmyorderflow';
+import {createloadingflow} from './loading';
 
 export default function* rootSaga() {
+  yield fork(createloadingflow);
   yield fork(wsrecvsagaflow);
   yield fork(flowmain);
   yield fork(payflow);
---
+++ app/src/sagas/loading.js
@@ -0,0 +1,104 @@
+import { select,put,takeEvery,race,take,call,takeLatest} from 'redux-saga/effects';
+import {delay} from 'redux-saga';
+import {
+    set_weui,
+    login_result,
+    getmyborrowusers_request//例子《----
+} from '../actions';
+import { push } from 'react-router-redux';
+import _ from 'lodash';
+
+const getloginstate = (state) => {
+  const {loginsuccess} = state.userlogin;
+  return {
+    loginsuccess
+  };
+}
+
+export function* createloadingflow(){
+  let action_request = (action)=>{
+    let actiontype = action.type;
+    return _.endsWith(actiontype,'_request');
+  }
+
+  yield takeEvery(action_request, function*(actionreq) {
+    let actionstringsz = _.split(actionreq.type,/[ _]/);
+    let actionstring = actionstringsz[actionstringsz.length - 2];//肯定大于1，因为已经判断有_了
+    if(actionstring === 'loginwithtoken' || actionstring==='loginwithauth'){
+      actionstring = 'login';
+    }
+
+    if(actionstring !== 'login' && actionstring !== 'logout'){
+      let action_result = (action)=>{
+        let actiontype = action.type;
+        let isresult = _.endsWith(actiontype,`${actionstring}_result`);
+        if(isresult){
+          return true;
+        }
+        return false;
+      }
+
+      let action_commonerr = (action)=>{
+        let actiontype = action.type;
+        let iscommon_err = _.endsWith(actiontype,'common_err');
+        if(iscommon_err){
+          const {payload} = action;
+          if(!!payload){
+            return payload.type === actionstring;
+          }
+        }
+        return false;
+      }
+
+      const { result,err, timeout } = yield race({
+          result: take(action_result),
+          err: take(action_commonerr),
+          timeout: call(delay, 500)
+      });
+
+      if(!!timeout){
+        //超过500毫秒才弹
+        yield put(set_weui({
+            loading : {
+                show : true
+            },
+        }));
+
+        const { result,err, timeout } = yield race({
+            result: take(action_result),
+            err: take(action_commonerr),
+            timeout: call(delay, 2000)
+        });
+
+        if(!!timeout){
+          console.log(`这是什么请求，要等那么多时间===>${actionstring}`);
+        }
+
+        yield put(set_weui({
+            loading : {
+                show : false
+            },
+        }));
+      }
+    }
+    else{
+      console.log(`这个请求就不要loading了===>${actionstring}`);
+    }
+
+  });
+
+  //===========************===========
+  yield takeLatest(
+    [
+      `${getmyborrowusers_request}`,//例子《《-------
+    ]
+    , function*(action) {
+      //目的地地址选中后
+      const {loginsuccess} = yield select(getloginstate);
+      if(!loginsuccess){
+        yield take(`${login_result}`);
+        yield put(action);
+        console.log(`等待登录后才发送：${JSON.stringify(action.type)}`);
+      }
+    });
+}
